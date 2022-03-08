6199f663ae67a4b3c36de5ce79b6dd471381867f
vagner da silva <vagnrdaia@gmail.com>
2022-03-07 23:35:46 -0300

    add Transaction Table to Dashboard

--- src/style/global.ts
+++ src/style/global.ts
@@ -4,6 +4,7 @@ export const GlobalStyle = createGlobalStyle`
     :root{
         --red:#E52E4D;
         --blue:#5429CC;
+        --green:#33CC95;
         
         --blue-light:#6933FF;
         
@@ -50,4 +51,4 @@ export const GlobalStyle = createGlobalStyle`
         opacity: 0.6;
         cursor:not-allowed;
     }
-`
+`;
