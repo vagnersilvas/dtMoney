520760f67d12013ae3712d4dc0e34f3c64d43645
vagner da silva <vagnrdaia@gmail.com>
2022-03-08 22:08:13 -0300

    update name files styles

--- src/components/TransactionsTable/styles.ts
+++ src/components/TransactionsTable/styles.ts
@@ -33,17 +33,5 @@ export const Container = styled.div`
         color: var(--red);
       }
     }
-    /* tr {
-      display: flex;
-      justify-content: space-between;
-      padding: 1.2rem 2rem;
-      margin: 0.5rem 0;
-      border-radius: 0.25rem;
-      transition: filter 0.2s;
-
-      &:hover {
-        filter: brightness(0.9);
-      }
-    } */
   }
 `;
