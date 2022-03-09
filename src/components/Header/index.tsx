def30adaa5d227fd8875ed833081e9a278ac21ac
vagner da silva <vagnrdaia@gmail.com>
2022-03-08 23:09:33 -0300

    add NewTransactions to Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,20 @@
 import logoImg from "../../assets/Logo.svg";
-import { Container, Content } from "./style";
+import { Container, Content } from "./styles";
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void;
+}
 
-export function Header() {
+export function Header({ onOpenNewTransactionModal }: HeaderProps) {
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="dt money" />
-        <button type="button">Nova Transação</button>
+        <button
+          onClick={onOpenNewTransactionModal}
+          type="button">
+          Nova Transação
+        </button>
+
       </Content>
     </Container>
   );
