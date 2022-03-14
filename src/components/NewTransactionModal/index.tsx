41b804422c5f9d0b742b8fe5ed979f76561549f0
vagner da silva <vagnrdaia@gmail.com>
2022-03-13 21:47:24 -0300

    add states set data request api

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,16 +1,40 @@
+import { FormEvent, useState } from "react";
+import { api } from "../../services/api";
+
 import Modal from "react-modal";
+
 import { Container, ContainerButtonTransaction, RadioBox } from "./styles";
+
 import buttonClose from '../../assets/buttonClose.svg'
 import incomeImg from '../../assets/incomes.svg';
 import outcomeImg from '../../assets/outComes.svg';
-import { useState } from "react";
+
 interface NewTransactionModalProps {
     isOpen: boolean;
     onRequestClose: () => void;
 }
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
-
+    const [title, setTitle] = useState('');
+    const [value, setValue] = useState(0);
+    const [category, setCategory] = useState('');
     const [type, setType] = useState('deposit');
+
+    function handleCreateNewTransacion(event: FormEvent) {
+        event.preventDefault();
+        
+        setCategory('');
+        setTitle('');
+        setValue(0);
+
+        const data ={
+            title,
+            value,
+            category,
+            type
+        }
+
+        api.post('/transactions',data);
+    }
 
     return (
         <Modal
@@ -24,48 +48,71 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
                 onClick={onRequestClose}
                 className="react-modal-close"
             >
-                <img src={buttonClose} alt="Fechar modal" />
+                <img
+                    src={buttonClose}
+                    alt="Fechar modal"
+                />
             </button>
 
-            <Container>
+            <Container onSubmit={handleCreateNewTransacion}>
                 <h2>Cadastrar transação</h2>
 
                 <input
                     type="text"
                     placeholder="Título"
+                    value={title}
+                    onChange={event => setTitle(event.target.value)}
                 />
+
                 <input
                     type="number"
                     placeholder="Valor"
+                    value={value}
+                    onChange={event => setValue(Number(event.target.value))}
                 />
+
                 <ContainerButtonTransaction>
+
                     <RadioBox
                         type="button"
                         className="button-transaction income"
+
                         onClick={() => setType('deposit')}
                         isActive={type === 'deposit'}
                         activeColor="green"
                     >
-                        <img src={incomeImg}
-                            alt="Entrada" />
+                        <img
+                            src={incomeImg}
+                            alt="Entrada"
+                        />
                         <span>Entrada</span>
+
                     </RadioBox>
+
                     <RadioBox
                         type="button"
                         className="button-transaction outcome"
+
                         onClick={() => setType('withdraw')}
-                        isActive={ type === 'withdraw'}
+                        isActive={type === 'withdraw'}
                         activeColor="red"
-                        >
+                    >
                         <img
                             src={outcomeImg}
-                            alt="Saida" />
+                            alt="Saida"
+                        />
                         <span>Saída</span>
                     </RadioBox>
+
                 </ContainerButtonTransaction>
+
                 <input
                     type="text"
-                    placeholder="Categoria" />
+                    placeholder="Categoria"
+                    value={category}
+                    onChange={event => setCategory(event.target.value)}
+                />
+
                 <button type="submit">
                     Cadastrar
                 </button>
