1e33ad39fb5c69e04abb2587d9804d2af5b7c536
vagner da silva <vagnrdaia@gmail.com>
2022-03-13 21:49:10 -0300

    format styles plugin colorize

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -2,17 +2,19 @@ import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
     :root{
-        --red:#E52E4D;
-        --blue:#5429CC;
-        --green:#33CC95;
+        --red: #E52E4D;
+        --blue: #5429CC;
+        --green: #33CC95;
         
-        --blue-light:#6933FF;
+        --blue-light: #6933FF;
         
-        --text-title:#363F5F;
-        --text-body:#969CB2;
+        --text-title: #363F5F;
+        --text-body: #969CB2;
         
-        --background:#F0F2F5;
+        --background: #F0F2F5;
         --shape: #FFFFFF;
+
+        --border-input: #d7d7d7;
     }
 
     html{
